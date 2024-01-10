2530324b1f6a8e1926d2374600afcce96b0f96db
Mathis Jibert <mathis.jibert@gmail.com>
2024-01-10 15:09:39 +0100

    PoliceCars

--- Assets/_Scripts/Mathis/PoliceCarBehavior.cs
+++ Assets/_Scripts/Mathis/PoliceCarBehavior.cs
@@ -3,38 +3,11 @@ using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
-public class PoliceCarBehavior : MonoBehaviour
+public class PoliceCarBehavior : CarBehaviour
 {
-    [Header("REFERENCES")] 
-    [SerializeField] public Rigidbody rb;
-    [SerializeField] public Wheel[] wheels;
-    [SerializeField] private Transform target;
-    
-    [Header("SUSPENSION")] 
-    [SerializeField] private float suspensionLenght = 0.5f;
-    [SerializeField] private float suspensionForce = 70f;
-    [SerializeField] private float suspensionDampening = 3f;
-    [SerializeField] private float anchoring = 0.2f;
-    
-    [Header("WHEEL")] 
-    [SerializeField] private float wheelMass = 0.1f;
-    [SerializeField] private float maxSpeed = 25f;
-    [SerializeField] private AnimationCurve accelerationBySpeedFactor;
-    [SerializeField] private float acceleration = 12f;
-    [SerializeField] private float braking = 12f;
-    [SerializeField] private float decceleration = 4f;
-    [SerializeField] private AnimationCurve steeringByTriggerFactor;
-    [SerializeField] private float steeringSpeed = 50f;
-    
-    [Header("PHYSICVALUES")]
-    [SerializeField] private Vector3 localCenterOfMass;
-    
-    [Header("BRAKEDRIFT")] 
-    [SerializeField] private float dampeningMultiplier = 0.25f;
-    [SerializeField] private float steeringMultiplier = 0.95f;
-    [SerializeField] private float accelMultiplier = 1.5f;
-    [SerializeField] private float angleMinToExitDrift = 0.1f;
-    
+    [Header("POLICE CAR")]
+    public Transform target;
+
     [Header("WALLBOUNCE")]
     [Tooltip("Le pourcentage de vitesse gardée lors d'un wallBounce")]
     [SerializeField] private float speedRetained = 0.7f;
@@ -44,151 +17,28 @@ public class PoliceCarBehavior : MonoBehaviour
     [SerializeField] private float minAngleToBounce = 0.3f;
     [SerializeField] private GameObject fxBounce;
     
-    private bool driftEngaged;
-    private float driftValue;
-    private float dirCam;
-
-    // INPUT VALUES
-    private Vector2 stickValue;
-    public float accelForce, brakeForce;
-    private bool driftBrake;
-
-    public float rotationValue;
-    
-    private void Start()
-    {
-        rb.centerOfMass = localCenterOfMass;
-    }
-    
     private void Update()
     {
-        
-
-        rotationValue = -Mathf.Clamp(Vector2.SignedAngle(new Vector2(transform.forward.x,transform.forward.z),
+        float angleToTarget = Vector2.SignedAngle(new Vector2(transform.forward.x, transform.forward.z),
             new Vector2(target.position.x, target.position.z) -
-            new Vector2(transform.position.x, transform.position.z)) / 10,-1,1);
-        
-        Debug.DrawRay(transform.position + Vector3.up *3,new Vector2(transform.forward.x,transform.forward.z),Color.red);
-        Debug.DrawRay(transform.position + Vector3.up *3,new Vector2(target.position.x, target.position.z) - new Vector2(transform.position.x, transform.position.z),Color.cyan);
-        
-        //Debug.Log(rb.velocity.magnitude);
-        for (int i = 0; i < wheels.Length; i++)
-        {
-            if (wheels[i].steeringFactor > 0)
-            {
-                wheels[i].wheelVisual.localRotation = wheels[i].transform.localRotation = 
-                    Quaternion.Lerp(wheels[i].transform.localRotation,Quaternion.Euler(0,rotationValue * wheels[i].steeringFactor * (driftBrake ? steeringMultiplier : 1),0),Time.deltaTime * steeringSpeed);
-            }
-        }
-        
-        driftValue = 1- Mathf.Abs(Vector3.Dot(new Vector3(rb.velocity.normalized.x,0,rb.velocity.normalized.z), transform.forward));
+            new Vector2(transform.position.x, transform.position.z));
 
-        // SORTIE DU DRIFT BRAKE SI ON SE REALIGNE AVEC LA VELOCITE
-        if (driftBrake)
-        {
-            if (!driftEngaged && driftValue > angleMinToExitDrift + 0.1f)
-            {
-                driftEngaged = true;
-            }
-            else if (driftEngaged && driftValue < angleMinToExitDrift)
-            {
-                driftBrake = false;
-                driftEngaged = false;
-                
-            }
-        }
+        rotationValue = -Mathf.Clamp(angleToTarget / 10,-1,1);
+        
+        if(angleToTarget > 90 || angleToTarget < -90) driftBrake = true;
+        
+        OnMove();
+
     }
     
     void FixedUpdate()
     {
-        for (int i = 0; i < wheels.Length; i++)
-        {
-            // APPLICATION DES FORCES
-            Vector3 wheelForce = GetWheelForces(wheels[i]);
-            rb.AddForceAtPosition(wheelForce,wheels[i].transform.position);
-        }
+        ApplyWheelForces();
     }
 
-    #region Wheel Methods
-    Vector3 GetWheelForces(Wheel wheel)
-    {
-        Vector3 wheelForce = Vector3.zero;
-
-        // SI ROUE AU SOL, ALORS FORCES
-        if (Physics.Raycast(wheel.transform.position, -wheel.transform.up, out RaycastHit hit, suspensionLenght + anchoring))
-        {
-            // CALCUL DES FORCES
-            float suspension = GetWheelSuspensionForce(wheel,hit);
-            float directionalDamp = GetWheelDirectionalDampening(wheel);
-            float drivingForce = wheel.drivingFactor > 0 ? GetWheelAcceleration(wheel) : 0;
-            wheelForce = wheel.transform.up * suspension +
-                         wheel.transform.right * directionalDamp +
-                         wheel.transform.forward * drivingForce ;
-            
-            // DEBUG RAYS
-            Debug.DrawRay(wheel.transform.position,wheel.transform.up * suspension,Color.green);
-            Debug.DrawRay(wheel.transform.position,wheel.transform.right * directionalDamp,Color.red);
-            Debug.DrawRay(wheel.transform.position,wheel.transform.forward * drivingForce,Color.blue);
-            Debug.DrawRay(wheel.transform.position,wheelForce,Color.white);
-        }
-
-        return wheelForce;
-    }
-    
-    float GetWheelSuspensionForce(Wheel wheel,RaycastHit hit)
-    {
-        float force = 0;
-        
-        float offset = suspensionLenght - hit.distance;
-        Vector3 wheelWorldVelocity = rb.GetPointVelocity(wheel.transform.position);
-        float velocity = Vector3.Dot(wheel.transform.up, wheelWorldVelocity);
-        force = (offset * suspensionForce) - (velocity * suspensionDampening);
-        wheel.wheelVisual.position = wheel.transform.position - wheel.transform.up * (hit.distance - 0.25f);
-        
-        return force;
-    }
-    
-    float GetWheelDirectionalDampening(Wheel wheel)
-    {
-        float force = 0;
-        
-        Vector3 wheelWorldVelocity = rb.GetPointVelocity(wheel.transform.position);
-        float tangentSpeed = Vector3.Dot(wheelWorldVelocity,wheel.transform.right);
-        float counterAcceleration = (-tangentSpeed * wheel.directionalDampening) / Time.fixedDeltaTime;
-
-        force = wheelMass * counterAcceleration * (driftBrake ? dampeningMultiplier : 1);
-        
-        return force;
-    }
-    
-    float GetWheelAcceleration(Wheel wheel)
-    {
-        float force = 0;
-
-        float factor = rb.velocity.magnitude / maxSpeed;
-        
-        float accel = accelForce * acceleration * accelerationBySpeedFactor.Evaluate(factor) * wheel.drivingFactor * (driftBrake ? accelMultiplier : 1);
-
-        float brake = 0;
-        if (Vector3.Dot(rb.velocity, transform.forward) > 0.1f)
-        {
-            brake = brakeForce * -braking * wheel.drivingFactor * (driftBrake ? 0 :1);
-        }
-        else
-        {
-            brake = brakeForce * -decceleration * wheel.drivingFactor;
-        }
-
-        force = accel + brake;
-        
-        return force;
-    }
-    
-    #endregion
-    
     private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.CompareTag("Wall"))
+        if (other.gameObject.CompareTag("Wall") || other.gameObject.CompareTag("Cone"))
         {
             Debug.Log(other.relativeVelocity.magnitude);
             if (Vector3.Dot(other.contacts[0].normal, transform.forward) < -minAngleToBounce)
