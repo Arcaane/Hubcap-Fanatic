33cba2631713c156488ac89b4edb43d5d9a3cd0e
Mathis Jibert <mathis.jibert@gmail.com>
2024-01-16 14:31:00 +0100

    Merchant Part 2

--- Assets/MerchantBehavior.cs
+++ Assets/MerchantBehavior.cs
@@ -1,18 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MerchantBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public NavMeshAgent agent;
+    public Transform[] points;
+    public float speed;
+    public int index;
     void Update()
     {
-        
+        if (Vector2.SqrMagnitude(new Vector2(transform.position.x, transform.position.z) -
+                                 new Vector2(points[(index + 1) % points.Length].position.x,
+                                     points[(index + 1) % points.Length].position.z)) < 25)
+        {
+            index = (index + 1) % points.Length;
+            agent.SetDestination(points[(index + 1) % points.Length].position);
+        }
+    }
+
+    private void Start()
+    {
+        agent.SetDestination(points[(index + 1) % points.Length].position);
     }
 }
