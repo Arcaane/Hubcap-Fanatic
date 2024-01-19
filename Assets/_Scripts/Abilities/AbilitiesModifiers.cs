90a09d689240a9ca20209aa66dd03c95594edcf6
Mathis Jibert <mathis.jibert@gmail.com>
2024-01-19 15:08:59 +0100

    Fixs de bugs

--- Assets/_Scripts/Abilities/AbilitiesModifiers.cs
+++ Assets/_Scripts/Abilities/AbilitiesModifiers.cs
@@ -3,12 +3,6 @@ using UnityEngine;
 
 namespace Abilities
 {
-    [Serializable]
-    public struct ActiveModifier
-    {
-        public AbilitiesStats stat;
-        public float newValue;
-    }
     
     [CreateAssetMenu(menuName = "AbilityModifier", fileName = "New AbilityModifier")]
     public class AbilitiesModifiers : ScriptableObject
