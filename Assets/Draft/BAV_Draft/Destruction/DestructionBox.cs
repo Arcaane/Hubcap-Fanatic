070c5ba0adfb2de9c4deb256e5c05c04c850c55e
BaptisteV <baptiste.valle98@gmail.com>
2024-02-06 15:08:24 +0100

    Destruction

---
+++ Assets/Draft/BAV_Draft/Destruction/DestructionBox.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class DestructionBox : MonoBehaviour
+{
+    [Header("Renderer")] 
+    [SerializeField] private GameObject box;
+    [SerializeField] private GameObject fracturedBox;
+    
+    [Header("Explosion")]
+    [SerializeField] private float explosionForce = 100;
+    [SerializeField] private float explosionRadius = 10;
+    
+    
+    [Header("Debug")]
+    public Collider[] colliders;
+    public Rigidbody[] rigidbodies;
+    private BoxCollider boxCollider;
+    
+    private Transform player;
+    private bool hasExploded = false;
+    
+    public void Start()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+        if (CarController.instance != null)
+        {
+            player = CarController.instance.transform;
+        }
+        else
+        {
+            Debug.LogError("CarController.instance is not set correctly!");
+        }
+        EnableCollider();
+        EnableKinematics();
+        fracturedBox.SetActive(false);
+    }
+
+    void EnableCollider()
+    {
+        if (colliders == null) AddCollidersToArray();
+        foreach (var collider in colliders)
+        {
+            collider.enabled = true;
+        }
+    }
+
+    void EnableKinematics(bool value = true)
+    {
+        if (rigidbodies == null) AddRBToArray();
+        foreach (var rb in rigidbodies)
+        {
+            rb.isKinematic = value;
+        }
+    }
+    
+    [ContextMenu("Setup/Setup Box")]
+    void SetupBox()
+    {
+        AddCollidersToArray();
+        AddRBToArray();
+    } 
+    
+    
+    [ContextMenu("Setup/Clear")]
+    void ClearArray()
+    {
+        colliders = null;
+        rigidbodies = null;
+    }
+    
+    void AddCollidersToArray()
+    {
+        colliders = GetComponentsInChildren<Collider>();
+    } 
+    
+    void AddRBToArray()
+    {
+        rigidbodies = GetComponentsInChildren<Rigidbody>();
+    }
+    
+    public void DestroyBox()
+    {
+        ReplaceWithFracturedBox();
+        EnableKinematics(false);
+        foreach (var rigidbody in rigidbodies)
+        {
+            rigidbody.isKinematic = false;
+            rigidbody.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+        }
+        EnableTriggerAsync();
+        DestroyObjectAsync();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !hasExploded)
+        {
+            hasExploded = true;
+            DestroyBox();
+            ReplaceWithFracturedBox();
+        }
+    }
+
+    async void EnableTriggerAsync()
+    {
+        await Task.Delay(250);
+        foreach (var col in colliders)
+        {
+            col.isTrigger = false;
+        }
+    }
+
+    async void DestroyObjectAsync()
+    {
+        await Task.Delay(5000); 
+        if (hasExploded)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    void ReplaceWithFracturedBox()
+    {
+        if (fracturedBox != null)
+        {
+            fracturedBox.SetActive(true);
+            box.SetActive(false);
+            boxCollider.enabled = false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+    }
+}
